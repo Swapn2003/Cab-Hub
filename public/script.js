0dae54331a4fdf280e6315c7d5b2cdacb1fa9675
Maharsh04 <r.maharsh@iitg.ac.in>
2023-07-11 13:15:15 +0530

    Uber Done

---
+++ public/script.js
@@ -0,0 +1,99 @@
+// ------------------------------------------------Setting Map------------------------------------------------
+mapboxgl.accessToken = 'pk.eyJ1Ijoici1tYWhhcnNoIiwiYSI6ImNsanF4cDI4ZDA5Y2ozbG80MWplMW5tMmkifQ.a1bn9Rbia-YVClKGHN2N3g';
+
+// Creating map object
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+// Adding zoom and rotation controls
+map.addControl(new mapboxgl.NavigationControl(),'bottom-right');
+
+// Adding Directions control
+let directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+    unit: 'metric',
+    profile: 'mapbox/driving'
+});
+map.addControl(directions, 'top-left');
+
+let originInput = document.querySelector('#origin');
+let destinationInput = document.querySelector('#destination');
+let cabFares = document.querySelector('.cab-fares');
+
+function searchLocation(modeId) {
+    cabFares.innerHTML="";
+    let origin = originInput.value;
+    let destination = destinationInput.value;
+    directions.setOrigin(origin);
+    directions.setDestination(destination);
+    setTimeout(function () {
+        let originCoordinates = directions.getOrigin().geometry.coordinates;
+        let destinationCoordinates = directions.getDestination().geometry.coordinates;
+        geocodeCoordinates(originCoordinates, function (originPlaceName) {
+            geocodeCoordinates(destinationCoordinates, function (destinationPlaceName) {
+                console.log('Origin:', originPlaceName);
+                console.log('Destination:', destinationPlaceName);
+                const data = {
+                    originPlaceName:originPlaceName,
+                    destinationPlaceName: destinationPlaceName
+                };
+                
+                    fetch('http://localhost:5000/Uberprice', {
+                        method: 'POST',
+                        headers: {
+                        'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    }).then(response => response.json())
+                        .then(result => {
+                        // Handle the response result
+                        result.forEach(info=>{
+                            let fare=document.createElement('div');
+                            let cab=document.createElement('h5');
+                            let price=document.createElement('p');
+                            let book=document.createElement('button');
+                            book.innerText='Login to book';
+                            book.addEventListener('click',()=>{
+                                window.location.href=info.url;
+                            })
+                            cab.innerText=info.name;
+                            price.innerText=info.fare;
+                            fare.appendChild(cab);
+                            fare.appendChild(price);
+                            fare.appendChild(book);
+                            cabFares.appendChild(fare);
+                        });
+                        console.log(result);
+                        })
+                        .catch(error => {
+                        // Handle any errors
+                        console.error(error);
+                        });
+            });
+        });
+    }, 2000);
+    
+    // ------------------------------------------------UI update------------------------------------------------
+    
+    const modes=document.querySelectorAll('.modes');
+    for(let mode of modes){
+        mode.classList.remove('active');
+    }
+    document.querySelector(`#${modeId}`).classList.add('active');
+    
+};
+
+function geocodeCoordinates(coordinates, callback) {
+    let geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${coordinates[0]},${coordinates[1]}.json?access_token=${mapboxgl.accessToken}`;
+    fetch(geocodingUrl)
+        .then(response => response.json())
+        .then(data => {
+            let placeName = data.features[0].place_name;
+            callback(placeName);
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+}
